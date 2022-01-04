d215b7c68e72188eb9b93fc198e62c801ef58c3a
TakeenJ <takeen.jazayeri@yahoo.com>
2022-01-04 17:09:46 +0330

    Defining the generation of first population

--- genetic.py
+++ genetic.py
@@ -1,3 +1,24 @@
+import random as rn
+
+class Chromosome:
+    def __init__ (self, length):
+        self.genes = [0 for i in range(length+1)]
+
+def random_chro (adj_matrix, length):
+    new_chro = Chromosome(length)
+
+    for i in range(1, length+1):
+        neighbors = []
+        for j in range(length+1):
+            if adj_matrix[i][j] == 1:
+                neighbors.append(j)
+        new_chro.genes[i] = neighbors[rn.randint(0, len(neighbors)-1)]
+    
+    return new_chro
+
+def genetic (adj_matrix, popu, iter_num):
+    skip
+
 file_address = input('add file address:\n')
 try:
     file = open(file_address, 'r')
@@ -5,12 +26,12 @@ except IOError:
     print('There is a problem with file address.')
 
 first_line = file.readline()
-nodes_number = int(first_line[:-1])
+nodes_num = int(first_line[:-1])
 
 adj_matrix = []
-for i in range(nodes_number+1):
+for i in range(nodes_num+1):
     new_row = []
-    for j in range(nodes_number+1):
+    for j in range(nodes_num+1):
         new_row.append(0)
     adj_matrix.append(new_row)
 
@@ -21,6 +42,6 @@ while i < len(file_content):
     adj_matrix[x][y] = 1
     adj_matrix[y][x] = 1
     i += 2
+file.close()
 
-for j in range(len(adj_matrix)):
-    print(adj_matrix[j])
+print(random_chro(adj_matrix, nodes_num).genes)
