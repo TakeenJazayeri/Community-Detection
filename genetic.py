2876d1b396010843695805a27b99609220849d80
TakeenJ <takeen.jazayeri@yahoo.com>
2022-01-04 19:41:33 +0330

    Calculating fitness function for individuals

--- genetic.py
+++ genetic.py
@@ -1,8 +1,66 @@
 import random as rn
 
+class Partition:
+    def __init__ (self, num):
+        self.parent = []
+        for i in range(num+1):
+            self.parent.append(i)
+    
+    def find(self, node):
+        return self.parent[node]
+    
+    def union (self, node1, node2):
+        a = self.find(node1)
+        b = self.find(node2)
+        for i in range(len(self.parent)):
+            if self.parent[i] == b:
+                self.parent[i] = a
+
+    def report(self):
+        result = [[] for i in range(len(self.parent))]
+        for i in range(1, len(self.parent)):
+            result[self.find(i)].append(i)
+
+        j = 0
+        while j < len(result):
+            if result[j] == []:
+                result.pop(j)
+            else:
+                j += 1
+        return result
+    
+
 class Chromosome:
     def __init__ (self, length):
         self.genes = [0 for i in range(length+1)]
+    
+    def Q (self, adj_matrix, length):
+        m = 0
+        k = [0 for i in range(length+1)]
+        for i in range(length+1):
+            for j in range(length+1):
+                if adj_matrix[i][j] == 1:
+                    k[i] += 1
+                    m += 0.5
+
+        p = Partition(length)
+        for i in range(length+1):
+            p.union(i, self.genes[i])
+        print(p.report())
+
+        result = 0
+        for i in range(1, length+1):
+            for j in range(1, length+1):
+                if p.find(i) == p.find(j):
+                    result += adj_matrix[i][j] - (k[i] * k[j] / (2 * m))
+        result /= (2 * m)
+        if result < 0:
+            return 0
+        elif result > 1:
+            return 1
+        return result
+
+
 
 def random_chro (adj_matrix, length):
     new_chro = Chromosome(length)
@@ -16,8 +74,12 @@ def random_chro (adj_matrix, length):
     
     return new_chro
 
-def genetic (adj_matrix, popu, iter_num):
-    skip
+def genetic (adj_matrix, popu, iter_num, length):
+    chro_list = []
+    for i in range(popu):
+        chro_list.append(random_chro(adj_matrix, length))
+    
+    
 
 file_address = input('add file address:\n')
 try:
@@ -43,5 +105,3 @@ while i < len(file_content):
     adj_matrix[y][x] = 1
     i += 2
 file.close()
-
-print(random_chro(adj_matrix, nodes_num).genes)
