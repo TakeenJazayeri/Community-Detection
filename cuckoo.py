42764af1b8666f1e3d50553023a280d6192592bf
TakeenJ <takeen.jazayeri@yahoo.com>
2022-01-11 16:55:54 +0330

    Cuckoo spawning and deleting bad eggs

--- cuckoo.py
+++ cuckoo.py
@@ -16,9 +16,9 @@ class Partition:
             if self.parent[i] == b:
                 self.parent[i] = a
     
-    def build_partition (self, hab, length):
+    def build_partition (self, cuc, length):
         for i in range(1, length+1):
-            selected_ngh = ngh_matrix[i][hab.coordinates[i]]
+            selected_ngh = ngh_matrix[i][cuc.habitat[i]]
             self.union(i, selected_ngh)
 
     def report(self):
@@ -35,9 +35,28 @@ class Partition:
         return result
     
 
-class Habitat:
+class Cuckoo:
     def __init__ (self, length):
-        self.coordinates = [0 for i in range(length+1)]
+        self.habitat = [0 for i in range(length+1)]
+    
+    def difference (self, other, length):
+        dif_sum = 0
+        for i in range(length+1):
+            dif = self.habitat[i] - other.habitat[i]
+            if dif >= 0:
+                dif_sum += dif
+            else:
+                dif_sum -= dif
+        return dif_sum
+
+    def spawn (self, adj_matrix, ngh_matrix, my_egg_num, my_ELR, length):
+        my_egg_list = []
+        while len(my_egg_list) < my_egg_num:
+            new_cuc = random_cuc(adj_matrix, ngh_matrix, length)
+            if self.difference(new_cuc, length) < my_ELR:
+                my_egg_list.append(new_cuc)
+
+        return my_egg_list
     
     def Q (self, adj_matrix, length):
         m = 0
@@ -64,11 +83,6 @@ class Habitat:
             return 1
         return result
     
-    def difference (self, other, length):
-        dif = []
-        for i in range(length+1):
-            dif.append(self.coordinates[i] - other.coordinates[i])
-    
 
 
 def find_neighbors (node_num, adj_matrix, length):
@@ -78,20 +92,79 @@ def find_neighbors (node_num, adj_matrix, length):
             neighbors.append(i)
     return neighbors
 
-def random_hab (adj_matrix, ngh_matrix, length):
-    new_hab = Habitat(length)
+def random_cuc (adj_matrix, ngh_matrix, length):
+    new_cuc = Cuckoo(length)
     for i in range(1, length+1):
         neighbors = find_neighbors(i, adj_matrix, length)
-        new_hab.coordinates[i] = random.randint(0, len(ngh_matrix[i])-1)
-    return new_hab
+        new_cuc.habitat[i] = random.randint(0, len(ngh_matrix[i])-1)
+    return new_cuc
 
+def set_egg_and_ELR (var_low, var_high, alpha, popu):
+    egg_num = []
+    total_num = 0
+    ELR = []
+
+    for i in range(popu):
+        egg_num.append(random.randint(var_low, var_high))
+        total_num += egg_num
+    
+    for i in range(popu):
+        ELR[i] = alpha * (egg_num[i] / total_num) * (var_high - var_low)
+    
+    return egg_num, ELR
+
+def two_sort (arr1, arr2):
+    if len(arr1) > 1:
+        mid = len(arr1)//2
+        L1 = arr1[:mid]
+        L2 = arr2[:mid]
+        R1 = arr1[mid:]
+        R2 = arr2[mid:]
+        two_sort(L1, L2)
+        two_sort(R1, R2)
+  
+        i = j = k = 0
+        while i < len(L1) and j < len(R1):
+            if L1[i] < R1[j]:
+                arr1[k] = L1[i]
+                arr2[k] = L2[i]
+                i += 1
+            else:
+                arr1[k] = R1[j]
+                arr2[k] = R2[j]
+                j += 1
+            k += 1
+  
+        while i < len(L1):
+            arr1[k] = L1[i]
+            arr2[k] = L2[i]
+            i += 1
+            k += 1
+  
+        while j < len(R1):
+            arr1[k] = R1[j]
+            arr2[k] = R2[j]
+            j += 1
+            k += 1
 
 
     
-def cuckoo_algorithm (adj_matrix, popu, iter_num, length):
-    hab_list = []
+def cuckoo_algorithm (adj_matrix, ngh_matrix, popu, iter_num, var_low, var_high, alpha, length):
+    cuc_list = []
     for i in range(popu):
-        hab_list.append(random_hab(adj_matrix, length))
+        cuc_list.append(random_cuc(adj_matrix, length))
+    
+    egg_num, ELR = set_egg_and_ELR (var_low, var_high, alpha, popu)
+
+    egg_list = []
+    for i in range(popu):
+        for egg in cuc_list[i].spawn(adj_matrix, ngh_matrix, egg_num[i], ELR[i], length):
+            egg_list.append(egg)
+    
+    egg_list_Q = []
+    for i in range(popu):
+        egg_list_Q.append(egg_list[i].Q(adj_matrix, length))
+    two_sort(egg_list_Q, egg_list)
 
     
 
@@ -125,7 +198,13 @@ ngh_matrix = []
 for i in range(nodes_num+1):
     ngh_matrix.append(find_neighbors(i, adj_matrix, nodes_num))
 
-print(adj_matrix)
-print(ngh_matrix)
-for i in range(10):
-    print(random_hab(adj_matrix, ngh_matrix, nodes_num).coordinates)
+#print(adj_matrix)
+#print(ngh_matrix)
+#for i in range(10):
+#    print(random_cuc(adj_matrix, ngh_matrix, nodes_num).habitat)
+
+arr1 = [3, 5, -1, 4, 2, 11, 10]
+arr2 = [1, 2, 3, 4, 5, 6, 7]
+two_sort(arr1, arr2)
+print(arr1)
+print(arr2)
